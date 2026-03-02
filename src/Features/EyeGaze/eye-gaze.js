ce5f5d160e0389d4c7eb744a70f565db60a09e3c
GabrielRalph <gltralph@gmail.com>
2026-03-02 21:13:03 +1100

    seperate files

--- src/Features/EyeGaze/eye-gaze.js
+++ src/Features/EyeGaze/eye-gaze.js
@@ -1,23 +1,12 @@
 import { Vector } from "../../SvgPlus/4.js";
-import { AccessButton, AccessEvent } from "../../Utilities/Buttons/access-buttons.js";
-import { GridIcon } from "../../Utilities/Buttons/grid-icon.js";
-import { HideShowTransition } from "../../Utilities/hide-show.js";
-import { SvgResize } from "../../Utilities/svg-resize.js";
-import { relURL, dotGrid, argmin } from "../../Utilities/usefull-funcs.js";
+import { AccessEvent } from "../../Utilities/Buttons/access-buttons.js";
 import { addProcessListener, startProcessing, startWebcam, stopProcessing } from "../../Utilities/webcam.js";
 import { Features, SquidlyFeatureWindow } from "../features-interface.js";
 import { load } from "./Algorithm/index.js";
 import { CalibrationFrame } from "./calibration-frame.js";
 import { FeedbackWindow } from "./feedback-frame.js";
 import { addPointToHeatmaps, Heatmap } from "./heatmap.js";
-
-
-class CalibrationScreenArea extends SquidlyFeatureWindow {
-   constructor(){
-        super("calibration-window");
-        this.calibrationFrame = this.createChild(CalibrationFrame)
-   }
-}
+import { TestScreen } from "./test-screen.js";
 
 function clampV(v, min, max) {
     return new Vector(
@@ -30,133 +19,12 @@ function clampV0_1(v) {
     return clampV(v, new Vector(0, 0), new Vector(1, 1));
 }
 
-class TestScreen extends SquidlyFeatureWindow {
-    dotSize = 0.08;
-    constructor(){
-        super("test-screen", new HideShowTransition("test-screen"));
-        let svg = this.createChild(SvgResize, {});
-        svg.shown = true;
-        this.svg = svg;
-        svg.addDrawable(this)
-
-        // let b = this.createChild(AccessButton, {
-        //     events: {
-        //         "access-click": (e) => this.dispatchEvent(new AccessEvent("close", e))
-        //     },
-        //     class: "close",
-        // }, "test-close");
-        // b.createChild(Icon, {}, "close");
-        // b.createChild("div", {content: "Close"});
-        this.createChild(GridIcon, {}, {
-            type: "action",
-            displayValue: "Close",
-            symbol: "close",
-            events: {
-                "access-click": (e) => this.dispatchEvent(new AccessEvent("close", e))
-            }
-        })
+class CalibrationScreenArea extends SquidlyFeatureWindow {
+   constructor(){
+        super("calibration-window");
+        this.calibrationFrame = this.createChild(CalibrationFrame)
    }
-
-
-   hide(){
-        this.root.hide();
-        this.svg.stop();
-        this.shownUser = null;
-   }
-
-   async showFor(user) {
-        this.shownUser = user;
-        this.svg.start();
-        await this.root.show();
-   }
-
-    setEyeData(pos, user) {
-        if (this.shownUser == user) {
-            this.eyePosition = pos;
-        }
-    }
-
-    draw(){
-        let {svg} = this;
-        svg.innerHTML = "";
-        let width = this.clientWidth;
-        let height = this.clientHeight;
-        let diag = Math.sqrt(width**2 + height**2);
-        let dotSize = this.dotSize * diag;
-        let mx = dotSize * 0.75;
-        let my = dotSize * 0.75;
-        
-        let dots = dotGrid(3, new Vector(mx, my), new Vector(width -mx, my), new Vector(mx, height - my), new Vector(width - mx, height - my));
-        let dFurthest = Math.max((width-2*mx)/3, (height-2*my)/3);
-        let p;
-        let iClosest = -1;
-        if (this.eyePosition) {
-            p = this.eyePosition.mul(width, height);
-            iClosest =  argmin(dots.map(d => d.sub(p).norm()));
-        }
-
-        dots.forEach((pos, i) => {
-            if (i != 7) {
-                let dot = svg.createChild("g", {
-                    class: "dot",
-                    transform: `translate(${pos.x} ${pos.y})`,
-                });
-
-                dot.createChild("circle", { 
-                    class: "big",
-                    cx: 0,
-                    cy: 0,
-                    r: (dotSize/2),
-                });
-                dot.createChild("circle", { 
-                    class: "small",
-                    cx: 0,
-                    cy: 0,
-                    r: (dotSize/2) * 0.1,
-                });
-
-                if (i == iClosest) {
-                    let dist = 1 - p.sub(pos).norm() / dFurthest;
-                    if (dist < 0) dist = 0;
-
-                    let r = ( 0.1 + 0.45 * dist) * dotSize / 2;
-                    let stroke = dotSize * dist * 0.45;
-                    dot.createChild("circle", { 
-                        class: "proximity",
-                        cx: 0,
-                        cy: 0,
-                        r: r,
-                        "stroke-width": stroke,
-                    });
-                }
-            }
-        });
-    }
-
-    static get usedStyleSheets() {
-        return [relURL("./styles.css", import.meta), GridIcon.styleSheet]
-    }
 }
-
-class RestAccessButton extends AccessButton {
-    isPointInElement(p) {
-        let [pos,size] = this.bbox;
-        return p.y > pos.y; 
-    }
-
-    get disableSwitch() {return true}
-}
-
-class RestButton extends SquidlyFeatureWindow {
-    constructor() {
-        super("rest-button");
-        this.button = this.createChild(RestAccessButton, {class: "rest", content: "rest"})
-    }
-    static get usedStyleSheets() {
-        return [relURL("./styles.css", import.meta)]
-    }
-}
-
 
 export default class EyeGazeFeature extends Features {
     /**@type {CalibrationFrame} */
